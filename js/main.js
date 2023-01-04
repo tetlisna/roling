486b462814d3f6ad15e80631c25fade5efc6dfd9
Your Name <you@example.com>
2023-01-04 20:06:49 +0200

    sec

--- js/main.js
+++ js/main.js
@@ -15,17 +15,22 @@
 	const burgerItem = document.querySelector(".burger");
 	const menu = document.querySelector(".header__nav");
 	const menuClose = document.querySelector(".header__nav-close");
+	const menuLinks = document.querySelectorAll(".header__link");
 
 	burgerItem.addEventListener("click", () => {
-		console.log("1");
 		menu.classList.add("header__nav-active");
 	});
-	// menuClose.addEventListener("click", () => {
-	//     menu.classList.add('header__nav-active')
-	// });
+
 	menuClose.addEventListener("click", () => {
 		menu.classList.remove("header__nav-active");
 	});
+	if (window.innerWidth < 768) {
+		for (let i = 0; i < menuLinks.length; i += 1) {
+			menuLinks[i].addEventListener("click", () => {
+				menu.classList.remove("header__nav-active");
+			});
+		}
+	}
 })();
 
 // Scroll to anchors
